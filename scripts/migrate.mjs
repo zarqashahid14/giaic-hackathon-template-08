7b7975f7ce1a165780e732d7f5a1dd84c22d95fa
hamzah syed <hamzah.syed17@gmail.com>
2025-01-15 00:22:21 +0500

    updated the api url

--- scripts/migrate.mjs
+++ scripts/migrate.mjs
@@ -9,7 +9,7 @@ const {
   NEXT_PUBLIC_SANITY_PROJECT_ID, // Sanity project ID
   NEXT_PUBLIC_SANITY_DATASET, // Sanity dataset (e.g., "production")
   NEXT_PUBLIC_SANITY_AUTH_TOKEN, // Sanity API token
-  BASE_URL = "http://localhost:3000", // API base URL for products and categories
+  BASE_URL = "https://giaic-hackathon-template-08.vercel.app", // API base URL for products and categories
 } = process.env;
 
 // Check if the required environment variables are provided
